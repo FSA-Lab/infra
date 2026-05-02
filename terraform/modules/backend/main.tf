b44a7a3ec3b76ed438e2afcb5960b55e7373cae6
vtvinh24 <vtvinh24@gmail.com>
2026-05-02 16:00:50 +0700

    Setup AKS provisioning with remote tfstate

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -5,7 +5,6 @@ resource "azurerm_resource_group" "this" {
 
 resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
-  id                       = azurerm_resource_group.this.id
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
