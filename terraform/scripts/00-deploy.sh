1ac59cd45a5d0cd79b8a3d5d6512f86c90403aad
vtvinh24 <vtvinh24@gmail.com>
2026-05-05 13:19:02 +0700

    Fix paths in 00-deploy.sh

--- terraform/scripts/00-deploy.sh
+++ terraform/scripts/00-deploy.sh
@@ -3,14 +3,15 @@ set -e pipefail
 
 # This script is used to deploy Azure infrastructure using Terraform.
 
+HOME_DIR=$(pwd)
 DISPOSABle=true # This variable controls whether to destroy the infrastructure before deploying (excluding the remote state backend). Set to true currently for faster prototyping.
 
 # By default, run 01-base.sh, 02-aks.sh, and 03-app.sh sequentially.
 echo "Deploying base infrastructure..."
-bash $ROOT_DIR/terraform/scripts/01-base.sh
+bash $HOME_DIR/terraform/scripts/01-base.sh
 
 echo "Deploying AKS cluster..."
-bash $ROOT_DIR/terraform/scripts/02-aks.sh
+bash $HOME_DIR/terraform/scripts/02-aks.sh
 
 echo "Deploying app services..."
-bash $ROOT_DIR/terraform/scripts/03-app.sh
+bash $HOME_DIR/terraform/scripts/03-app.sh
