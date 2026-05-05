7bbe6ad66288034c2f5117ac4f99b5e808d13693
vtvinh24 <vtvinh24@gmail.com>
2026-05-05 11:15:39 +0700

    Fix typo in apply-infra.sh

--- scripts/apply-infra.sh
+++ scripts/apply-infra.sh
@@ -4,7 +4,7 @@ set -e pipefail
 ROOT_DIR=$(pwd)
 
 # Call terraform/scripts/deploy.sh to deploy the infrastructure
-bash $ROOT_DIR/terraform/scripts/deploy.sh
+bash $ROOT_DIR/terraform/scripts/00-deploy.sh
 
 ## Infra is up, including the AKS cluster. 
 
